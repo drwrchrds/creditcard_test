ffcd7086923285a0d2d86a2cfec58cfb6eb2868e
Andrew Richards <drew.rchrds@gmail.com>
2014-04-22 11:04:44 -0700

    comment code

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ You may need to install rspec first: 'gem install rspec'
 To run the program with your own input, please type this in input.txt (in the root directory). Then, in terminal, run 'ruby generate_report.rb'. The summary will be printed out.
 
 ### Design Decisions
+I was not familiar with Luhn 10 before this project. I looked this up and found a few mathematical implementations I could use, but I decided to use a Ruby gem made for this exact purpose - https://github.com/rolfb/luhn-ruby
+
 I wanted to modularize the card and processor functionality of this program, so that changes may be made to cards in the future that will not affect the processor, and vice-versa. The card class handles tracking of balance and user information, and validation. The processor is in charge of handling user input / output, and accessing a card list (this is how I imagine a CC processor works in real life).
 
 ### Why Ruby?
--- lib/braintree.rb
+++ lib/braintree.rb
@@ -3,16 +3,19 @@ require 'luhn'
 
 class Card 
   attr_reader :name, :limit, :balance, :number
-   
+  
   def initialize(name, number, limit)
     @name, @number, @limit = name, Integer(number), Integer(limit[1..-1])
+    # start new cards with 0 balance
     @balance = 0
   end
   
+  # check Luhn validation, and check that card no. has no more than 19 digits
   def number_valid?
     @number.to_s.length < 20 && Luhn.valid?(@number)
   end
   
+  # display balance, or display error
   def balance
     number_valid? ? "$#{@balance}" : 'error'
   end
@@ -23,13 +26,14 @@ class Card
       @balance += amount unless @balance + amount > limit
     end
   end
-  
+
   def credit(amount)
     if number_valid?
       @balance -= Integer(amount[1..-1])
     end
   end
   
+  # this gets called by 'puts'
   def to_s
     "#{name}: #{balance}"
   end
@@ -43,6 +47,8 @@ class Processor
     @cards = {}
   end
   
+  # add a new card to the @cards hash
+  # with 'name' as key and card object as value
   def add(name, number, limit)
     card = Card.new(name, number, limit)
     cards[name] = card
@@ -60,6 +66,9 @@ class Processor
     cards.keys.sort.each { |name| puts cards[name] }
   end
   
+  # get the command from the first word of the line
+  # make it a symbol and 'send' it to self
+  # include the rest of the arguments splatted, because there may be two or three
   def input(string)
     input = string.split(' ')
     method = input.shift.downcase.to_sym
